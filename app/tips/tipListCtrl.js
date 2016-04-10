c770acaeef35908e8f337f340eef8acb63ca99e8
evagithub <evadraganova@hotmail.com>
2016-04-09 20:51:31 -0400

    add tips list

---
+++ app/tips/tipListCtrl.js
@@ -0,0 +1,104 @@
+var app;
+(function (app) {
+    var tipApp;
+    (function (tipApp) {
+        var TipListCtrl = (function () {
+            function TipListCtrl() {
+                this.title = "Some Tips";
+                this.showDetails = false;
+                this.tips = [{
+                        "name": "Relax Eating",
+                        "shortDescription": "Relax and eat your food slowly.",
+                        "description": "Instead of eating a lot at one sitting, which can make acid-reflux symptoms worse, eat only until you're comfortably full (versus overly stuffed). Be sure to sit down and take your time during a meal; strive to truly taste and enjoy each mouthful. One tried-and-true trick to eating more slowly is to put down your fork on the table between every few bites.",
+                        "image": "relax.png",
+                        "rating": 6
+                    },
+                    {
+                        "name": "Limit Beverages",
+                        "shortDescription": "Limit beverages during meals.",
+                        "description": "If you suffer from GERD, limit your fluid intake with meals. Liquids add to the volume of food in your stomach and increases stomach distension. A full belly puts more pressure on the lower esophageal sphincter (LES), the muscle that normally prevents food from moving back up into your esophagus, and thus adds to your risk of reflux. To minimize stomach volume, take small sips of water while you eat, and try to drink mostly between rather than during meals.",
+                        "image": "beverage.png",
+                        "rating": 6
+                    },
+                    {
+                        "name": "Keep weight",
+                        "shortDescription": "Shed extra pounds.",
+                        "description": "Being overweight is a major contributor to heartburn. Overweight individuals are twice as likely to suffer from symptoms of GERD as are people of a healthy weight. Extra weight may increase pressure on the stomach, causing the LES muscle to relax, which allows stomach backflow. Body fat may also release chemicals that interfere with normal digestive functioning. The good news is that research shows losing even a small amount of weight can help relieve symptoms and control heartburn. If you need help getting started, check out my four-step online weight-loss program, which provides daily meal plans, healthy recipes, access to a personal nutrition coach, and handy trackers to help you succeed.",
+                        "image": "weight.png",
+                        "rating": 5
+                    },
+                    {
+                        "name": "Exercise",
+                        "shortDescription": "Incorporate moderate exercise.",
+                        "description": "The key word here is moderate. Vigorous exercises like running can agitate your digestive tract and provoke reflux, but incorporating moderate, low-impact exercises such as walking is actually beneficial (and it keeps you upright, allowing gravity to aid digestion). Exercise can also help you lose weight, which can dramatically reduce the severity of heartburn. To minimize the risk of symptoms, wait at least two hours after eating to work out; also avoid sports drinks, which may aggravate reflux due to their acidity. If you're a beginner, start slow and build up to at least 30 minutes of walking most days of the week.",
+                        "image": "excersise.png",
+                        "rating": 5
+                    },
+                    {
+                        "name": "Less Fat",
+                        "shortDescription": "Cut the fat.",
+                        "description": "Lay off the chicken wings, fried foods, deep-dish pizza, and marbled steak. High-fat meals like these relax the LES and delay stomach emptying, making it more likely that you'll experience reflux. Instead, make lean proteins (like skinless poultry, seafood, beans, and lean cuts of red meat) and fiber-rich produce and whole grains your diet staples.",
+                        "image": "fat.png",
+                        "rating": 5
+                    },
+                    {
+                        "name": "Trigger Food Watch",
+                        "shortDescription": "Watch out for potential trigger foods.",
+                        "description": "When it comes to your diet, eating smaller meals is by far the most important step you can take to prevent reflux, more so than changing the foods you eat. That said, there are specific foods that can aggravate symptoms in certain individuals. The most common culprits are fried or fatty foods, alcohol, caffeinated beverages like coffee and tea, chocolate, mints and mint-flavored items, citrus juices and fruits, tomatoes and tomato sauce, spicy foods, onions, garlic, and carbonated beverages. Everyone has different triggers, though, so if you find that these foods don't cause you any discomfort, you can continue to enjoy them.",
+                        "image": "foodwatch.png",
+                        "rating": 4
+                    },
+                    {
+                        "name": "After eating",
+                        "shortDescription": "Don't lie down after eating.",
+                        "description": "If you're prone to acid reflux, it's important that you don't lie flat for a minimum of two hours after you've eaten a meal or snack. In fact, stand up and walk around to help encourage the gastric juices to flow in the right direction. For the same reason, while sleeping, keep your upper body elevated by placing a few books or a foam wedge underneath the mattress or propping up the legs at the head of the bed with blocks or books.",
+                        "image": "aftereating.png",
+                        "rating": 2
+                    },
+                    {
+                        "name": "Loose Clothing",
+                        "shortDescription": "Wear loose fitting clothing.",
+                        "description": "Avoid anything tight around your middle, which can put pressure on your stomach and worsen the discomfort and pain of GERD. Until your symptoms are under control, think stretchy and comfortable!",
+                        "image": "looseclothing.png",
+                        "rating": 2
+                    },
+                    {
+                        "name": "Cinamon Gum",
+                        "shortDescription": "Chew cinnamon gum after meals.",
+                        "description": "Chewing gum stimulates saliva production, which helps to neutralize stomach acid. Gum chewing also encourages frequent swallowing, which clears irritating acid from the esophagus more quickly. Just make sure to choose non-mint flavors, since peppermint and spearmint can relax the LES and exacerbate symptoms. Pick up cinnamon or fruit flavors instead.",
+                        "image": "cinamongum.png",
+                        "rating": 3
+                    },
+                    {
+                        "name": "No Smoking",
+                        "shortDescription": "Quit smoking.",
+                        "description": "Need another reason to give up cigarettes? Smoking increases your risk for GERD! Smoking slows digestion and increases stomach acid, while it also limits salvia production � your body's natural defense against stomach acid! Besides harming your esophagus, cigarette smoke also damages the digestive system and weakens your stomach's LES muscle, which directly causes acid reflux.",
+                        "image": "smoking.png",
+                        "rating": 6
+                    },
+                    {
+                        "name": "Overeating",
+                        "shortDescription": "Steer clear of late-night meals or big meals.",
+                        "description": "Avoid eating meals two to three hours before bedtime to reduce stomach acid and allow the stomach to partially empty its contents before you sleep, suggests the American Gastroenterological Association. Because large meals put pressure on your stomach, try eating a smaller meal in the evening to help prevent nighttime heartburn symptoms.",
+                        "image": "overeating.png",
+                        "rating": 5
+                    },
+                    {
+                        "name": "Sleep Left Side",
+                        "shortDescription": "Sleep on your left side",
+                        "description": "This position seems to help reduce nighttime heartburn symptoms.",
+                        "image": "sleepposition.png",
+                        "rating": 1
+                    }
+                ];
+            }
+            TipListCtrl.prototype.toggleDetails = function () {
+                this.showDetails = !this.showDetails;
+            };
+            return TipListCtrl;
+        }());
+        angular
+            .module("tipApp")
+            .controller("TipListCtrl", TipListCtrl);
+    })(tipApp = app.tipApp || (app.tipApp = {}));
+})(app || (app = {}));
